0bb8d333ba70a0599092cd5008aa9079b92c557e
Joe Van Kirk <imjoevankirk@gmail.com>
2017-12-12 11:36:44 -0700

    Root to pages#home and route /list to 'pages#list'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
 
-  get 'pages/list'
+  get 'list', to: 'pages#list'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
