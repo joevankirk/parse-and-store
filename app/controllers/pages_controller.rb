19cee3aef538d461c3027a0c2625576d96d28945
Joe Van Kirk <imjoevankirk@gmail.com>
2017-12-12 15:57:33 -0700

    Create new Page logic with Parsing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,39 @@
 class PagesController < ApplicationController
-  before_action :set_page, only:[:index, :create]
+  require 'open-uri'
 
   def index
     @pages = Page.all
   end
 
   def new
+    puts Dir.pwd
     @page = Page.new
+
   end
 
   def create
     @page = Page.new(page_params)
 
-    if @page.save
-      redirect_to links_path
+    if @page
+      parse(@page.url)
+      @page = Page.create(h1: @h1, h2: @h2, h3: @h3, links: @links, url: @page.url)
+      redirect_to list_path
     else
       render :new
     end
   end
 
-  def parse
+  def parse(url)
+    html_file = open(url).read
+    doc = Nokogiri::HTML(html_file)
 
+    @h1 = doc.css('h1').map(&:text).join(", ")
+    @h2 = doc.css('h2').map(&:text).join(", ")
+    @h3 = doc.css('h3').map(&:text).join(", ")
+    @links = doc.css('a').map(&:text).join(", ")
   end
 
   private
-
-  def set_page
-    @page = Page.find(params[:page_id])
-  end
 
   def page_params
     params.require(:page).permit(:url)
